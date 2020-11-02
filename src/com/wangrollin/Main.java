684b2b68dceba5e909d358d8967ffd1f9cac0b58
wangrollin <wangrollin@foxmail.com>
2020-11-02 19:40:49 +0800

    完善代码

--- src/com/wangrollin/Main.java
+++ src/com/wangrollin/Main.java
@@ -1,31 +1,28 @@
 package com.wangrollin;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-        generateTodolistOfYear(2020);
+        generateTodolistOfYear(2021);
     }
 
     private static void generateTodolistOfYear(int year) {
         String filePath = "out/" + year + ".md";
         try (OutputStream output = new FileOutputStream(filePath)) {
             System.out.println("# " + year + " todolist");
-            output.write(("# " + year + " todolist\n").getBytes());
+            output.write(("# " + year + " todolist\n\n").getBytes());
             Calendar calendar = Calendar.getInstance();
             calendar.set(year, 0, 1);
             while (calendar.get(Calendar.YEAR) == year) {
                 SimpleDateFormat format = new SimpleDateFormat("### yyyy.MM.dd 周u");
                 String curDate = format.format(calendar.getTime());
                 System.out.println(curDate);
-                output.write((curDate+"\n").getBytes());
+                output.write((curDate+"\n\n").getBytes());
                 calendar.add(Calendar.DATE, 1);
             }
         } catch (Exception e) {
